a98ae9184c6ffa32555a42b28568d1e6129cd091
Ashley Cho <ashleycho.slp@gmail.com>
2022-01-31 00:31:56 -0800

    edit api key/UV Index

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,63 +4,71 @@ var todayForecastEl = document.querySelector("#today-forecast");
 var fivedayForecastEl = document.querySelector("#fiveday-forecast");
 
 var apiKey = "7a3dbfc8c9397a95663108b4ab8a286e";
-var uvKey = "2888a7f888d86f8f76a218c861a7626e8668661";
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
-    
+
     var city = cityInputEl.value.trim();
 
     if (city) {
-      getForecast(city);
+        getForecast(city);
 
-      todayForecastEl.textContent = '';
-      cityInputEl.value = '';
+        todayForecastEl.textContent = '';
+        cityInputEl.value = '';
     } else {
-      alert("Please enter a valid city!");
+        alert("Please enter a valid city!");
     }
-  };
+};
 
-var getForecast = function() {
+var getForecast = function () {
     var city = $("#cityname").val();
-    
-    var apiUv = "https://api.geocod.io/v1.7/geocode?q=" + city + "&api_key=" + uvKey;
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
 
-    fetch(apiUv)
-    .then(function(uvResponse) {
-        return uvResponse.json();
-    })
-    .then(function(uvResponse) {
-        var latitude = uvResponse.results[0].location.lat;
-        var longitude = uvResponse.results[0].location.lng;
-
-        var currentCity = uvResponse.results[0].address_components.city;
-        var currentState = uvResponse.results[0].address_components.state;
-
-        var currentDate = moment().format('L');
-
-        $("#city-name").append(currentCity + ", " + currentState + "  (" + currentDate + ")");
-
-        fetch(apiUrl)
-        .then(function(response) {
+    fetch(apiUrl)
+        .then(function (response) {
             return response.json();
         })
 
-        .then(function(response) { 
+        .then(function (response) {
             var weatherIcon = response.current.weather[0].icon;
             var currentTemp = response.current.temp;
             var currentWind = response.current.wind_speed;
             var currentHumid = response.current.humidity;
-            var currentUV = response.current.uvi;
+
+            // Add $ to append to HTML
         })
-    }) 
-  };
+
+    var lat = uvResponse.data.coord.lat;
+    var lon = uvResponse.data.coord.lon;
+    var apiUv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid" + apiKey;
+
+    fetch(apiUv)
+        .then(function (uvResponse) {
+            return uvResponse.json();
+        })
+        .then(function (uvResponse) {
+            let uvIndex = uvResponse.value;
+
+            // Append uvIndex to HTML
+
+            if (uvIndex >= 0 && uvIndex < 3) {
+                uvIndex.setAttribute("class", "uv-favorable");
+            }
+            else if (uvIndex >= 3 && uvIndex < 8) {
+                uvIndex.setAttribute("class", "uv-moderate");
+            }
+            else {
+                uvIndex.setAttribute("class", "uv-severe");
+            }
+    })
+
+
+};
 
 
 
-  var displayResults = function(cities, searchTerm) {
+var displayResults = function (cities, searchTerm) {
 
-  };
+};
 
 userFormEl.addEventListener("submit", formSubmitHandler);
