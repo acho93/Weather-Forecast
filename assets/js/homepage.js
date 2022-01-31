a2070612c5e959affbbcf31d36fb6fdd3e517aed
Ashley Cho <ashleycho.slp@gmail.com>
2022-01-31 00:59:01 -0800

    UV Index

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,9 @@
 var cityInputEl = document.querySelector("#cityname");
-
 var todayForecastEl = document.querySelector("#today-forecast");
 var fivedayForecastEl = document.querySelector("#fiveday-forecast");
+
+// Add other constants
+const currentUvEl = document.getElementById("uvIndex");
 
 var apiKey = "7a3dbfc8c9397a95663108b4ab8a286e";
 
@@ -47,20 +49,21 @@ var getForecast = function () {
             return uvResponse.json();
         })
         .then(function (uvResponse) {
-            let uvIndex = uvResponse.value;
+            let uvIndex = document.createElement("span");
 
-            // Append uvIndex to HTML
-
-            if (uvIndex >= 0 && uvIndex < 3) {
+            if (uvResponse.data[0].value < 3) {
                 uvIndex.setAttribute("class", "uv-favorable");
             }
-            else if (uvIndex >= 3 && uvIndex < 8) {
+            else if (uvResponse.data[0].value >= 3 && uvResponse.data[0].value < 6) {
                 uvIndex.setAttribute("class", "uv-moderate");
             }
             else {
                 uvIndex.setAttribute("class", "uv-severe");
             }
-    })
+            uvIndex.innerHTML = uvResponse.data[0].value;
+            currentUvEl.innerHTML = "UV Index: ";
+            currentUvEl.append(uvIndex);
+    });
 
 
 };
