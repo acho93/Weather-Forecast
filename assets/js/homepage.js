6c302b0f232e61c0aa03b75586b866f071474eb2
Ashley Cho <ashleycho.slp@gmail.com>
2022-02-08 14:50:35 -0800

    fix api routes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,17 @@
+var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#cityname");
 var todayForecastEl = document.querySelector("#today-forecast");
 var fivedayForecastEl = document.querySelector("#fiveday-forecast");
 
 // Add other constants
-const currentUvEl = document.getElementById("uvIndex");
+const cityNameEl = document.getElementById("#city-name");
+const currentUvEl = document.getElementById("#uvIndex");
 
 var apiKey = "7a3dbfc8c9397a95663108b4ab8a286e";
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-
-    var city = cityInputEl.value.trim();
-
+    var city = $("#cityname").val();
     if (city) {
         getForecast(city);
 
@@ -22,8 +22,7 @@ var formSubmitHandler = function (event) {
     }
 };
 
-var getForecast = function () {
-    var city = $("#cityname").val();
+var getForecast = function (city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
 
     fetch(apiUrl)
@@ -32,46 +31,51 @@ var getForecast = function () {
         })
 
         .then(function (response) {
-            var weatherIcon = response.current.weather[0].icon;
-            var currentTemp = response.current.temp;
-            var currentWind = response.current.wind_speed;
-            var currentHumid = response.current.humidity;
+            console.log("HELLO", response)
+            var weatherIcon = response.weather[0].icon;
+            var currentTemp = response.main.temp;
+            var currentWind = response.wind.speed;
+            var currentHumid = response.main.humidity;
 
-            // Add $ to append to HTML
+            $("#today-forecast").addClass("#today-forecast");
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+
+            var apiUv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+            
+            fetch(apiUv)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                    let uvIndex = document.createElement("span");
+                    var uviData = response.daily[0].uvi 
+                    if (uviData < 3) {
+                        uvIndex.setAttribute("class", "uv-favorable");
+                    }
+                    else if (uviData >= 3 && uviData < 6) {
+                        uvIndex.setAttribute("class", "uv-moderate");
+                    }
+                    else {
+                        uvIndex.setAttribute("class", "uv-severe");
+                    }
+                    uvIndex.innerHTML = uviData;
+                    // var currentUvEl = $("#uvIndex")
+                    // currentUvEl.innerHTML = "UV Index: ";
+                    currentUvEl.textContent = "UV Index: ";
+                    currentUvEl.append(uvIndex);
+                });
+
         })
 
-    var lat = uvResponse.data.coord.lat;
-    var lon = uvResponse.data.coord.lon;
-    var apiUv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid" + apiKey;
-
-    fetch(apiUv)
-        .then(function (uvResponse) {
-            return uvResponse.json();
-        })
-        .then(function (uvResponse) {
-            let uvIndex = document.createElement("span");
-
-            if (uvResponse.data[0].value < 3) {
-                uvIndex.setAttribute("class", "uv-favorable");
-            }
-            else if (uvResponse.data[0].value >= 3 && uvResponse.data[0].value < 6) {
-                uvIndex.setAttribute("class", "uv-moderate");
-            }
-            else {
-                uvIndex.setAttribute("class", "uv-severe");
-            }
-            uvIndex.innerHTML = uvResponse.data[0].value;
-            currentUvEl.innerHTML = "UV Index: ";
-            currentUvEl.append(uvIndex);
-    });
 
 
 };
 
 
 
-var displayResults = function (cities, searchTerm) {
+// var displayResults = function (cities, searchTerm) {
 
-};
+// };
 
-userFormEl.addEventListener("submit", formSubmitHandler);
+searchFormEl.addEventListener("submit", formSubmitHandler);
